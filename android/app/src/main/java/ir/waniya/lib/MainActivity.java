c8238a194e9890f3a487484a487615848f308b4b
mohammadhosseiniparto <m.hosseini@asredanesh.com>
2023-06-17 19:01:51 +0330

    vaniya lib

---
+++ android/app/src/main/java/ir/waniya/lib/MainActivity.java
@@ -0,0 +1,5 @@
+package ir.waniya.lib;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,18 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'vaniya-lib',
+  appId: 'ir.waniya.lib',
+  appName: 'کتابخانه وانیا',
   webDir: 'www',
   server: {
     androidScheme: 'https'
   }
 };
 
+export interface PluginsConfig {
+  [CAPACITOR_ANDROID_STUDIO_PATH: string]: | {
+    [CAPACITOR_ANDROID_STUDIO_PATH: string]: '/home/mohammadhosseiniparto/Downloads/android-studio/bin/studio.sh';
+  }
+    | undefined;
+}
 export default config;
