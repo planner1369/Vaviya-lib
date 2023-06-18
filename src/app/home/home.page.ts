f1fc5466a0a3b425f13c9d998e8ddfba6a9c5240
mohammadhosseiniparto <m.hosseini@asredanesh.com>
2023-06-18 10:06:08 +0330

    vaniya lib

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import { Platform } from '@ionic/angular';
+declare var navigator: any;
+declare var window: any;
 
 @Component({
   selector: 'app-home',
@@ -7,14 +10,26 @@ import {Router} from "@angular/router";
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
-  constructor(private router: Router) {
+  subscription: any;
+  constructor(private router: Router, private platform: Platform) {
   }
 
   ngOnInit() {
   }
-
   goTo(to: string, id?: number) {
-    this.router.navigate([`${to}/${id}`]).then();
+    if (id) {
+      this.router.navigate([`${to}/${id}`]).then();
+    } else {
+      this.router.navigate([`${to}`]).then();
+    }
+  }
+  close(){
+    this.platform.ready().then(() => {
+      if (navigator.app) {
+        navigator.app.exitApp();
+      } else if (window.cordova) {
+        window.cordova.plugins.exit();
+      }
+    });
   }
 }
